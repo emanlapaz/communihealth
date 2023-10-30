da89225cabd3ebeb747d962aec0a65f0ea3e96cf
emanlapaz <eugeniojrmanlapaz@gmail.com>
2023-10-30 12:31:44 +0000

    Card View Map Added

--- app/src/main/java/com/example/communityhealth/activities/PatientMapsActivity.kt
+++ app/src/main/java/com/example/communityhealth/activities/PatientMapsActivity.kt
@@ -6,13 +6,19 @@ import com.google.android.gms.maps.GoogleMap
 
 import com.example.communityhealth.databinding.ActivityPatientMapsBinding
 import com.example.communityhealth.databinding.ContentPatientMapsBinding
+import com.example.communityhealth.main.MainApp
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class PatientMapsActivity : AppCompatActivity() {
+class PatientMapsActivity :AppCompatActivity(), GoogleMap.OnMarkerClickListener {
 
     private lateinit var binding: ActivityPatientMapsBinding
     private lateinit var contentBinding: ContentPatientMapsBinding
     lateinit var map: GoogleMap
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +26,15 @@ class PatientMapsActivity : AppCompatActivity() {
         binding = ActivityPatientMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+        app = application as MainApp
 
         contentBinding = ContentPatientMapsBinding.bind(binding.root)
         contentBinding.mapView.onCreate(savedInstanceState)
+
+        contentBinding.mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
 
     }
 
@@ -49,6 +61,20 @@ class PatientMapsActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         contentBinding.mapView.onSaveInstanceState(outState)
+    }
+    private fun configureMap() {
+        map.uiSettings.isZoomControlsEnabled = true
+        app.patients.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.MRN).position(loc)
+            map.setOnMarkerClickListener(this)
+            map.addMarker(options)?.tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
+    }
+    override fun onMarkerClick(marker: Marker): Boolean {
+        contentBinding.currentMRN.text = marker.title
+        return false
     }
 }
 
